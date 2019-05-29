6f5c92e783591d974564ed7c7ccb17dbc6c2e438
Roman Glozman <RomanGlozman92@gmail.com>
2019-05-29 11:54:10 +0300

    added project doc structure

--- mwsd/__init__.py
+++ mwsd/__init__.py
@@ -1,3 +1,16 @@
 from .version import __version__
 
 __all__ = ['tfidf', 'word2vec', 'utils']
+
+__doc__ = """
+Multi Writing Style Detection
+=====================================================================
+
+**mwsd** is a Python package providing an implementation of an algorithm to detect
+a Multi Writing Style Detection in texts.
+
+Main Features
+-------------
+  - Determining whether 2 texts are written in the same writing style.
+  - Determining whether a text is composed of two writing styles.
+"""
