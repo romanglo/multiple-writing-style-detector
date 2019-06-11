f39def43e9f0ddf3ab69e320a7390e85f9703c81
Roman Glozman <RomanGlozman92@gmail.com>
2019-06-11 19:25:16 +0300

    ZV and DZV updated and added time measurement

--- mwsd/run.py
+++ mwsd/run.py
@@ -2,9 +2,9 @@ from __future__ import division
 
 import logging
 import sys
+import time
 from builtins import int
 
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import ks_2samp
 
@@ -17,102 +17,133 @@ chunk_size = 50
 n_top_keywords = 1000
 
 
-def calculate_similarity_matrix(model, ids, words, chunk_size=chunk_size):
-    chunks = utils.chunks(list(zip(ids, words)), chunk_size)
+def calculate_similarity_matrix(model, words, chunk_size=chunk_size):
+    chunks = utils.chunks(words, chunk_size)
 
     logging.debug(f"No. of words: {len(words)}, Chunk size: {chunk_size}")
 
     chunks_num = int(
-        len(ids) / chunk_size) + (1 if (len(ids) % chunk_size != 0) else 0)
+        len(words) / chunk_size) + (1 if (len(words) % chunk_size != 0) else 0)
 
     logging.debug(f"No. of chunks {chunks_num}")
 
     similarites_vector_size = (chunk_size * (chunk_size - 1)) // 2
     similarites_matrix = np.zeros((chunks_num, similarites_vector_size))
 
-    logging.debug(f"Similarity matrix shape {similarites_matrix.shape}")
+    similarity_func = np.vectorize(
+        lambda x, y: model.similarity(x, y), otypes=[float])
 
     chunk_index = 0
     for chunk in chunks:
         in_chunk_index = 0
         for i in range(len(chunk)):
-            for j in range(i + 1, len(chunk)):
-                similarites_matrix[
-                    chunk_index][in_chunk_index] = model.similarity(
-                        chunk[i][1], chunk[j][1])
-                in_chunk_index += 1
+            similarities = similarity_func(chunk[i], chunk[i + 1:len(chunk)])
+            similarites_matrix[chunk_index, in_chunk_index:in_chunk_index +
+                               len(similarities)] = similarities
+            in_chunk_index += len(similarities)
+
         chunk_index += 1
+
+    logging.debug(f"Similarity matrix shape {similarites_matrix.shape}")
 
     return similarites_matrix
 
 
-def calculate_zv_distances(similarites_matrix, T=T, show_plt=False):
+def calculate_zv_distances(similarites_matrix, T=T):
     chunks_num = similarites_matrix.shape[0]
     ZVs = np.zeros(chunks_num - T + 1)
-    ZV_statistic = np.zeros((T))
 
-    for i in range(chunks_num - 1, T - 1, -1):
-        for j in range(i - 1, i - 1 - T, -1):
-            ZV_statistic[i - j - 1], _ = ks_2samp(similarites_matrix[i],
-                                                  similarites_matrix[j])
-        ZVs[i - T + 1] = np.sum(ZV_statistic)
-
-    if show_plt:
-        plt.clf()
-        plt.plot(ZVs)
-        plt.ylabel('ZV')
-        plt.show()
+    for i in range(ZVs.shape[0]):
+        ZVs[i] = np.sum(
+            np.apply_along_axis(
+                ks_2samp,
+                1,
+                similarites_matrix[i + 1:i + T + 1],
+                similarites_matrix[i],
+            )[:, 0],
+            axis=0)
 
     return ZVs
 
 
 def calculate_dzv_distances(first_similarites_matrix,
                             second_similarites_matrix,
-                            T=T,
-                            show_plt=False):
+                            T=T):
 
-    DZVs = np.zeros((first_similarites_matrix.shape[0] - T + 1,
-                     second_similarites_matrix.shape[0] - T + 1))
+    DZV = np.zeros((first_similarites_matrix.shape[0] - T + 1,
+                    second_similarites_matrix.shape[0] - T + 1))
 
-    for i in range(DZVs.shape[0]):
-        for j in range(i, DZVs.shape[1]):
-            zv_1 = 0
-            zv_2 = 0
-            zv_3 = 0
-            zv_4 = 0
-            for k in range(1, T + 1):
-                zv_1 += ks_2samp(first_similarites_matrix[i],
-                                 first_similarites_matrix[i + k])[0]
-                zv_2 += ks_2samp(first_similarites_matrix[j],
-                                 first_similarites_matrix[j + k])[0]
-                zv_3 += ks_2samp(first_similarites_matrix[i],
-                                 first_similarites_matrix[j + k])[0]
-                zv_4 += ks_2samp(first_similarites_matrix[j],
-                                 first_similarites_matrix[i + k])[0]
-            DZVs[i, j] = abs(zv_1 + zv_2 + zv_3 + zv_4)
+    for i in range(DZV.shape[0]):
 
-    # try:
-    #     i_upper = np.triu_indices(DZVs.shape[1])
-    #     DZVs.T[i_upper] = DZVs[i_upper]  # make the matrix symmetric
-    # except:
-    #     pass
+        zv_1 = np.sum(
+            np.apply_along_axis(
+                ks_2samp,
+                1,
+                first_similarites_matrix[i + 1:i + T + 1],
+                first_similarites_matrix[i],
+            )[:, 0],
+            axis=0)
 
-    return DZVs
+        for j in range(DZV.shape[1]):
+
+            zv_2 = np.sum(
+                np.apply_along_axis(
+                    ks_2samp,
+                    1,
+                    second_similarites_matrix[j + 1:j + T + 1],
+                    second_similarites_matrix[j],
+                )[:, 0],
+                axis=0)
+
+            zv_3 = np.sum(
+                np.apply_along_axis(
+                    ks_2samp,
+                    1,
+                    second_similarites_matrix[j + 1:j + T + 1],
+                    first_similarites_matrix[i],
+                )[:, 0],
+                axis=0)
+
+            zv_4 = np.sum(
+                np.apply_along_axis(
+                    ks_2samp,
+                    1,
+                    first_similarites_matrix[i + 1:i + T + 1],
+                    second_similarites_matrix[j],
+                )[:, 0],
+                axis=0)
+
+            DZV[i, j] = abs(zv_1 + zv_2 - zv_3 - zv_4)
+
+    return DZV
 
 
 def zv_process(first_text, second_text, model, stop_words, keywords):
+
+    start_time = time.time()
     ids, words = word2vec.text2ids(
         model=model,
         text=first_text + " " + second_text,
         stop_words=stop_words,
         acceptable_tokens=keywords,
         remove_skipped_tokens=True)
+    end_time = time.time()
+    logging.debug(f"word2vec runs {end_time-start_time:.4f} seconds")
 
-    sim_mat = calculate_similarity_matrix(model, ids, words)
+    start_time = time.time()
+    sim_mat = calculate_similarity_matrix(model, words)
+    end_time = time.time()
+    logging.debug(
+        f"similarity matrix calculation took {end_time-start_time:.4f} seconds"
+    )
 
     del ids, words
 
-    ZVs = calculate_zv_distances(sim_mat, show_plt=True)
+    start_time = time.time()
+    ZVs = calculate_zv_distances(sim_mat)
+    end_time = time.time()
+    logging.debug(
+        f"ZV distances calculation took {end_time-start_time:.4f} seconds")
 
     del sim_mat
 
@@ -121,51 +152,64 @@ def zv_process(first_text, second_text, model, stop_words, keywords):
 
 def dzv_process(first_text, second_text, model, stop_words, keywords):
 
+    start_time = time.time()
     first_text_ids, first_text_words = word2vec.text2ids(
         model=model,
         text=first_text,
         stop_words=stop_words,
         acceptable_tokens=keywords,
         remove_skipped_tokens=True)
+    end_time = time.time()
+    logging.debug(
+        f"first text word2vec runs {end_time-start_time:.4f} seconds")
 
-    first_sim_mat = calculate_similarity_matrix(model, first_text_ids,
-                                                first_text_words)
+    start_time = time.time()
+    first_sim_mat = calculate_similarity_matrix(model, first_text_words)
+    end_time = time.time()
+    logging.debug(
+        f"first text similarity matrix calculation took {end_time-start_time:.4f} seconds"
+    )
+
     del first_text_ids, first_text_words
 
+    start_time = time.time()
     second_text_ids, second_text_words = word2vec.text2ids(
         model=model,
         text=second_text,
         stop_words=stop_words,
         acceptable_tokens=keywords,
         remove_skipped_tokens=True)
+    end_time = time.time()
+    logging.debug(
+        f"second text word2vec runs {end_time-start_time:.4f} seconds")
 
-    second_sim_mat = calculate_similarity_matrix(model, second_text_ids,
-                                                 second_text_words)
+    start_time = time.time()
+    second_sim_mat = calculate_similarity_matrix(model, second_text_words)
+    end_time = time.time()
+    logging.debug(
+        f"second text similarity matrix calculation took {end_time-start_time:.4f} seconds"
+    )
+
     del second_text_ids, second_text_words
 
-    swapped = False
-
-    if first_sim_mat.shape[0] < second_sim_mat.shape[0]:
-        swapped = True
-        temp = first_sim_mat
-        first_sim_mat = second_sim_mat
-        second_sim_mat = temp
-
-    DZVs = calculate_dzv_distances(
+    start_time = time.time()
+    DZV = calculate_dzv_distances(
         first_similarites_matrix=first_sim_mat,
-        second_similarites_matrix=second_sim_mat,
-        show_plt=True)
+        second_similarites_matrix=second_sim_mat)
 
+    end_time = time.time()
+    logging.debug(
+        f"DZV matrix calculation took {end_time-start_time:.4f} seconds")
     del first_sim_mat, second_sim_mat
 
-    return DZVs, swapped
+    return DZV
 
 
 def process():
     # model_name = r"data\GoogleNews-vectors-negative300.bin"
 
     data_path = r"data\wiki_articles"
-    files = utils.get_files_list_from_dir(data_path)
+    files = utils.get_files_list_from_dir(data_path)[:6]
     texts = utils.read_text_from_files(files, encoding='latin-1')
     stop_words = utils.get_stop_words('english')
 
@@ -183,20 +227,23 @@ def process():
     first_text = " ".join(texts[:len(texts) // 2])
     second_text = " ".join(texts[len(texts) // 2:])
 
-    DZV, swapped = dzv_process(
+    ZV = zv_process(
         first_text=first_text,
         second_text=second_text,
         model=model,
         stop_words=stop_words,
         keywords=n_top_keyword)
 
-    # normalize
-    DZV *= 255 / DZV.max()
+    DZV = dzv_process(
+        first_text=first_text,
+        second_text=second_text,
+        model=model,
+        stop_words=stop_words,
+        keywords=n_top_keyword)
 
-    fig, ax = plt.subplots()
-    ax.matshow(DZV, cmap=plt.cm.Blues)
-    plt.show()
     del model
+
+    return ZV, DZV
 
 
 def main(argv):
